aa6d0786881eaefb6c40de120b2bf70a87beb188
Tristan Villanueva <tristanvillanueva@Mac.lan>
2025-02-02 11:03:41 -0800

    Menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
 function toggleMenu() {
     var menu = document.getElementById("nav-menu");
-
-    if (menu.style.display === "block") {
-        menu.style.display = "none"; // Hide menu
-    } else {
-        menu.style.display = "block"; // Show menu
-    }
+    menu.classList.toggle("show"); // Toggle the "show" class
 }
